abf786e106b12f65fd7cfb30f50b2a937f01d154
HypnoMama <sarahhermsen@gmail.com>
2018-08-17 00:43:20 +0000

    added instance variables to the show route

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,8 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @items = @order.line_items
+  
   end
 
   def create
@@ -66,5 +68,7 @@ class OrdersController < ApplicationController
     end
     total
   end
+
+ 
 
 end
