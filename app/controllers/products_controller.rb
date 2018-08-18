6ec3015ea53c76ac3b31e78c1fde2a1f643429ba
HypnoMama <sarahhermsen@gmail.com>
2018-08-18 02:15:46 +0000

    added instance variables for show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,14 @@
 class ProductsController < ApplicationController
-
+  
   def index
     @products = Product.all.order(created_at: :desc)
   end
 
   def show
-    @product = Product.find params[:id]
+    @product = Product.find(params[:id])
+    @reviews = Review.where({product_id: params[:id]}).order(created_at: :desc)    
+    @review = Review.new
   end
 
 end
+
