da9568265de0c5324d3e840d3a0a3f25e351fd8a
HypnoMama <sarahhermsen@gmail.com>
2018-08-20 23:14:23 +0000

    removed extra letter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User < ActiveRecord::Base
   
   validates :password, presence: true
   validates :password, presence: true, confirmation:  { case_sensitive: true }, length: { minimum: 8 } 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, confirmation: { case_sensitive: false }
   validates :password_confirmation, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
 
   def self.authenticate_with_credentials(email, password)
+    email = email.downcase.strip
+    password = password.strip
     @user = User.find_by(email: email) 
     if @user && @user.authenticate(password)    
       @user 
