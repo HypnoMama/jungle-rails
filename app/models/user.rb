e8e01ee9200a6554111cf39e0fa912356cd6fa47
HypnoMama <sarahhermsen@gmail.com>
2018-08-20 20:52:23 +0000

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
-
-  validates :first_name, presence: true
+  
   validates :password, presence: true
   validates :password, presence: true, confirmation:  { case_sensitive: true }, length: { minimum: 8 } 
-  validates :email, presence: true, confirmation: { case_sensitive: true }#, uniqueness: true
-    
+  validates :email, presence: true, uniqueness: true
+  validates :password_confirmation, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
 end
