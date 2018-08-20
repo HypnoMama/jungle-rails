415bb5cad362012745a28fd50c9cdb0cde4086cf
HypnoMama <sarahhermsen@gmail.com>
2018-08-20 20:51:43 +0000

    wrote all tests to validate user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe User, type: :model do
         expect(user).to_not be_valid
       end
     
-      it 'should not be valid with a short password' do 
+      it 'should not be valid with a password under 8 characters' do 
         user = User.new password: 'short', password_confirmation: 'short'
         expect(user).to_not be_valid 
       end
@@ -28,8 +28,22 @@ RSpec.describe User, type: :model do
         expect(user).to_not be_valid 
       end
 
-    
+      it 'should not be valid if email already exists' do 
+        user1 = User.new password: 'thisisapassword', password_confirmation: 'thisisapassword', email: 'test@test.com', first_name: 'alex', last_name: 'hungry'
+        user1.save! 
+        user2 = User.new password: 'passwordisthis', password_confirmation: 'passwordisthis', email: 'test@test.com', first_name: 'alex', last_name: 'hippo'
+        expect(user2).to_not be_valid
+      end
 
+      it 'should not be valid if first_name is not present' do 
+        user = User.new password: '123456789', password_confirmation:'123456789', email: 'test@test2.com', first_name: nil, last_name: "petey"
+        expect(user).to_not be_valid
+      end
+
+      it 'should not be valid if last_name is not present' do 
+        user = User.new password: '123456789', password_confirmation: '123456789', email: 'testy@test.com', first_name: 'alex'
+        expect(user).to_not be_valid
+      end
     end
  
   end
