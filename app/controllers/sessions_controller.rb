a13f4e471e7ba79249cc7934ab56ca29197ddc0e
HypnoMama <sarahhermsen@gmail.com>
2018-08-20 23:12:18 +0000

    removed dead code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # user = User.find_by email: params[:email]
-    # user = @user
-    # if user && user.authenticate(params[:password])
     if user = User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = user.id 
       redirect_to :products
