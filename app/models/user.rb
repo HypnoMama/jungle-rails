efbcf16a593a59174f58af9abd56abde27939db9
HypnoMama <sarahhermsen@gmail.com>
2018-08-20 21:23:56 +0000

    created class method authenticate with credentials on the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by(email: email) 
+    if @user.authenticate(password)    
+      @user 
+    else 
+      nil 
+    end
+  end
+
+
 end
